88aa323afb32b2e3de67fc6d9fbfb825e92ab106
Tuan Kiet <tuankietckcit@gmail.com>
2017-11-12 18:32:22 +0700

    Giao diện chấm công

--- GUI/ucQuyDinhLuong.cs
+++ GUI/ucQuyDinhLuong.cs
@@ -24,7 +24,6 @@ namespace GUI
             formatPhanTram();
 
             //giá trị mặc định
-            nudcongthang.Value = 24;
             nudCongNgay.Value = 8;
             nudBHYT_NV.Value = 2;
             nudBHXH_NV.Value = 4;
@@ -33,8 +32,6 @@ namespace GUI
         }
         public void formatPhanTram()
         {
-            nudcongthang.DecimalPlaces = 2;
-            nudcongthang.ThousandsSeparator = true;
 
             nudCongNgay.DecimalPlaces = 2;
             nudCongNgay.ThousandsSeparator = true;
